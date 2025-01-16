12b0076e896e2cc52af358b90bb9f2133f50260a
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2025-01-16 19:39:55 +0530

    Add files via upload

---
+++ January/Bitwise_xor.java
@@ -0,0 +1,23 @@
+package January;
+
+import java.util.*;
+
+public class Bitwise_xor {
+     public int xorAllNums(int[] nums1, int[] nums2) {
+          int n = nums1.length;
+          int m = nums2.length;
+          int xor1 = 0, xor2 = 0;
+
+          if (m % 2 == 1)
+               for (int i : nums1)
+                    xor1 = xor1 ^ i;
+          if (n % 2 == 1)
+               for (int i : nums2)
+                    xor2 = xor2 ^ i;
+          return xor1 ^ xor2;
+     }
+
+     public static void main(String[] args) {
+
+     }
+}
