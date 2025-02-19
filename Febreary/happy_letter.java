1b13760b5918ac771cbfbc3b0cf0c0315b35d984
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2025-02-19 14:44:26 +0530

    Add files via upload

---
+++ Febreary/happy_letter.java
@@ -0,0 +1,35 @@
+package Febreary;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class happy_letter {
+     public String getHappyString(int n, int k) {
+          Queue<String> q = new LinkedList<>();
+          q.offer("a");
+          q.offer("b");
+          q.offer("c");
+          List<String> happyStrings = new ArrayList<>();
+
+          while (!q.isEmpty()) {
+               String s = q.poll();
+               if (s.length() == n) {
+                    happyStrings.add(s);
+                    continue;
+               }
+               for (char ch : new char[] { 'a', 'b', 'c' }) {
+                    if (s.charAt(s.length() - 1) != ch) {
+                         q.offer(s + ch);
+                    }
+               }
+          }
+
+          return k > happyStrings.size() ? "" : happyStrings.get(k - 1);
+     }
+
+     public static void main(String[] args) {
+
+     }
+}
