3b475d453bf5ec4341a193ba163f47eb6d472ba0
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2025-03-03 20:14:49 +0530

    Add files via upload

---
+++ March/according.java
@@ -0,0 +1,40 @@
+package March;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class according {
+     public int[] pivotArray(int[] nums, int pivot) {
+          int n = nums.length;
+          List<Integer> list = new ArrayList();
+          int idx = 0;
+          int pivCnt = 0;
+
+          // Traverse the array to count pivot elements and segregate elements
+          for (int i = 0; i < n; i++) {
+               if (nums[i] == pivot) {
+                    pivCnt++;
+               } else if (nums[i] > pivot) {
+                    list.add(nums[i]);
+               } else {
+                    nums[idx++] = nums[i];
+               }
+          }
+
+          // Insert pivot elements
+          for (int i = 0; i < pivCnt; i++) {
+               nums[idx++] = pivot;
+          }
+
+          // Append greater elements
+          for (int val : list) {
+               nums[idx++] = val;
+          }
+
+          return nums;
+     }
+
+     public static void main(String[] args) {
+
+     }
+}
