05c87973843e8c6ee0c09640db8b51a69b12ea69
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2025-03-16 20:29:51 +0530

    Add files via upload

---
+++ March/repai.java
@@ -0,0 +1,34 @@
+package March;
+
+import java.util.Arrays;
+
+public class repai {
+     public boolean solve(long res, int[] ranks, int cars) {
+          long cnt = 0;
+          for (int rank : ranks) {
+               cnt += Math.sqrt((res * 1.0) / rank);
+          }
+          return cnt >= (long) cars;
+     }
+
+     public long repairCars(int[] ranks, int cars) {
+          long low = 1;
+          long high = (long) Arrays.stream(ranks).max().getAsInt() * cars * cars;
+          long ans = high;
+
+          while (low <= high) {
+               long mid = (low + high) / 2;
+               if (solve(mid, ranks, cars)) {
+                    ans = mid;
+                    high = mid - 1;
+               } else {
+                    low = mid + 1;
+               }
+          }
+          return ans;
+     }
+
+     public static void main(String[] args) {
+
+     }
+}
