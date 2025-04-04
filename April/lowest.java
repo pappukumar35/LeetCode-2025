636a0cc32ca18bc4e18e6d1735ef9b8a6a44e3bc
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2025-04-04 11:50:16 +0530

    Add files via upload

---
+++ April/lowest.java
@@ -0,0 +1,31 @@
+package April;
+
+public class lowest {
+     public TreeNode lcaDeepestLeaves(TreeNode root) {
+          int maxi = maxDepth(root); // maxi is the max depth
+          return dfs(root, maxi, 0);
+     }
+
+     private TreeNode dfs(TreeNode root, int maxi, int len) {
+          if (root == null)
+               return null;
+          if (maxi - 1 == len)
+               return root;
+          TreeNode left = dfs(root.left, maxi, len + 1);
+          TreeNode right = dfs(root.right, maxi, len + 1);
+
+          if (left != null && right != null)
+               return root;
+          return left != null ? left : right;
+     }
+
+     private int maxDepth(TreeNode root) {
+          if (root == null)
+               return 0;
+          return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+     }
+
+     public static void main(String[] args) {
+
+     }
+}
