acfd6f3a4e8375a6015156d19fb0285726fea639
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2025-04-05 19:58:55 +0530

    Add files via upload

---
+++ April/xor.java
@@ -0,0 +1,15 @@
+package April;
+
+public class xor {
+     public int subsetXORSum(int[] nums) {
+          int total = 0;
+          for (int num : nums) {
+               total |= num; // Step 1: Compute bitwise OR of all numbers
+          }
+          return total * (1 << (nums.length - 1)); // Step 2: Multiply by 2^(n-1)
+     }
+
+     public static void main(String[] args) {
+
+     }
+}
