58060d19486eb35e4346328340571f6f9129a3e3
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2025-04-27 19:59:05 +0530

    Add files via upload

---
+++ April/cot.java
@@ -0,0 +1,17 @@
+package April;
+
+public class cot {
+     public int countSubarrays(int[] nums) {
+          int count = 0;
+          for (int i = 1; i < nums.length - 1; ++i) {
+               if (nums[i] % 2 == 0 && nums[i] / 2 == nums[i - 1] + nums[i + 1]) {
+                    count++;
+               }
+          }
+          return count;
+     }
+
+     public static void main(String[] args) {
+
+     }
+}
