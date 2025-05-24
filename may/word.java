cc8a7173b332301eab768609703438cb4e10f11c
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2025-05-24 19:57:28 +0530

    Add files via upload

---
+++ may/word.java
@@ -0,0 +1,17 @@
+package may;
+
+public class word {
+     public List<Integer> findWordsContaining(String[] words, char x) {
+          List<Integer> res = new ArrayList<>(words.length);
+          for (int i = 0; i < words.length; ++i) {
+               if (words[i].indexOf(x) >= 0) {
+                    res.add(i);
+               }
+          }
+          return res;
+     }
+
+     public static void main(String[] args) {
+
+     }
+}
